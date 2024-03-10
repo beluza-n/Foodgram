35aa3d97a91c0e5ca54481f4cacd3baf3ff1b73f
beluza-n <beluza@mail.ru>
2024-03-11 01:36:30 +0300

    filter several tags

--- backend/recipe/filters.py
+++ backend/recipe/filters.py
@@ -5,7 +5,11 @@ from .models import Recipe
 class RecipeFilter(django_filters.FilterSet):
     is_favorited = django_filters.NumberFilter(field_name='is_favorited', method='filter_is_favorited')
     is_in_shopping_cart = django_filters.NumberFilter(field_name='is_in_shopping_cart', method='filter_is_in_shopping_cart')
-    tags = django_filters.CharFilter(field_name='tags__slug')
+    # tags = django_filters.CharFilter(field_name='tags__slug')
+    tags = django_filters.CharFilter(method='filter_tags')
+
+    def filter_tags(self, qs, name, value):
+        return qs.filter(tags__slug__in=self.request.GET.getlist('tags'))
 
     class Meta:
         model = Recipe
